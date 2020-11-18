2b1e25ae696453c386275e4fe5b88ebd7de7e9e1
carmim <carmim@gmail.com>
2020-11-18 15:04:20 +0200

    2**20 instead of 2**22

--- Q/mmu_x86/x86-32_va_to_pa.js
+++ Q/mmu_x86/x86-32_va_to_pa.js
@@ -11,11 +11,11 @@ function fillGarbage(page) {// Garbage looking as legit translation page
 	}
 }
 
-let va = Math.trunc(Math.random() * Math.pow(2,32));
-let pp = Math.trunc(Math.random() * Math.pow(2,22)) * 4096;
+let va = Math.trunc(Math.random() * (2**32));
+let pp = Math.trunc(Math.random() * (2**20)) * 4096;
 let pa = pp + (va % 4096);
 
-let i0 = Math.trunc(va / Math.pow(2, 22));
+let i0 = Math.trunc(va / (2**22));
 let i1 = Math.trunc(va / 4096) % 1024;
 
 let eVa = document.getElementById("va");
