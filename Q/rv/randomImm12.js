e536e43ffe653055749b3f474a423bedcca4937a
cs faculty of Tel-Aviv Academic College <59255650+csmtaacil@users.noreply.github.com>
2025-03-06 23:24:39 +0200

    Delete Q directory

--- Q/rv/randomImm12.js
+++
@@ -1,7 +0,0 @@
-export function randomImm12() {
-	let imm = -2048 + Math.trunc(Math.random() * 4096);
-	let immc = imm;
-	if (immc < 0)
-		immc = (2**12) + immc;
-	return ({str: imm.toString(10), code: immc});
-}
--- Q/rv/randomImm5.js
+++
@@ -1,4 +0,0 @@
-export function randomImm5() {
-	let imm = Math.trunc(Math.random() * 32);
-	return ({str: imm.toString(10), code: imm});
-}
